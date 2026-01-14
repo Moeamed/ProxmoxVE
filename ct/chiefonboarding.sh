eef8f42dd03098aa281bddc24504b1788e9ccbeb
Moeamed <mhamed.e@gmail.com>
2026-01-14 15:40:12 +0100

    Change source URL for build functions
    Updated the source URL for build functions in chiefonboarding.sh.

--- ct/chiefonboarding.sh
+++ ct/chiefonboarding.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/Moeamed/ProxmoxVE/refs/heads/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Claude (Assistant)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
